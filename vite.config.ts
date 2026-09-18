54ae9d43031f1ffd7cc062b6c6ecb3480888baac
pnrmaja <ponauerszilvi1@gmail.com>
2026-09-18 11:35:16 +0200

    github pages beallitasa

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,10 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/ZeneREACT/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
+    
   ],
 })
